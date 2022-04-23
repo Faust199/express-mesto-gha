779f8b6c43ba98b1ff428ff04f87c5ce9c20ddec
Nikita Polezhaev <polnikita199@yandex.ru>
2022-04-23 15:05:45 +0300

    eslint fixed

--- controllers/users.js
+++ controllers/users.js
@@ -81,7 +81,7 @@ module.exports.login = (req, res) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         })
-        .send({token});
+        .send({ token });
     })
     .catch((err) => {
       res
